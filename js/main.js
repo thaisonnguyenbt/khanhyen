08d0ff442e2949aa2953b0fa920eafb407618cb0
Thai Son Nguyen <thaison_nguyen@discovery.com>
2021-03-12 12:07:07 +0800

    update images

--- js/main.js
+++ js/main.js
@@ -276,7 +276,7 @@
 			var timeinterval = setInterval(updateClock, 1000);
 		}
 		// set your wedding date here
-		var deadline = 'November 28 2022 17:30:00 GMT+0300';
+		var deadline = 'April 10 2021 8:00:00 GMT+0700';
 		if (countdown){
 			initializeClock('timer', deadline);
 		}
