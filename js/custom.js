8be4197ae7d59e94e4d28b2359bd1f7579425ebc
Thai Son Nguyen <thaison_nguyen@discovery.com>
2021-03-16 20:39:44 +0800

    change pre-wedding header

--- js/custom.js
+++ js/custom.js
@@ -9,3 +9,29 @@ function toggleMusic() {
 setTimeout(function () {
   $('#background-music-button').removeClass('active');
 }, 10000);
+
+const changeIndexHeaderBg = (srcs) => {
+  setTimeout(() => {
+    const image = $('.parallax-slider');
+    const currentSrc = image.attr('src');
+
+    const index = srcs.indexOf(currentSrc);
+    let newSrc;
+    if (index === srcs.length - 1) {
+      newSrc = srcs[0];
+    } else {
+      newSrc = srcs[index + 1];
+    }
+
+    const clone = image.clone().insertBefore(image);
+    setTimeout(() => {
+      clone.remove();
+    }, 1000);
+
+    image.fadeOut(400, () => {
+      image.attr('src', newSrc).fadeIn(400);
+    });
+
+    changeIndexHeaderBg(srcs);
+  }, 5000);
+};
